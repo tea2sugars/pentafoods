256d522473b68d3852fdb6e81bab6f90967c591b
Rik Barwick <rik@creativitea.co.uk>
2015-05-18 12:07:03 +0100

    sync

---
+++ sites/default/files/js/js_uvFlBxjKNv3fQRiYPIf6d93kZqnC-XJYzKrQvro0Fa8.js
@@ -0,0 +1,206 @@
+(function ($) {
+
+$(document).ready(function() {
+
+  // Expression to check for absolute internal links.
+  var isInternal = new RegExp("^(https?):\/\/" + window.location.host, "i");
+
+  // Attach onclick event to document only and catch clicks on all elements.
+  $(document.body).click(function(event) {
+    // Catch the closest surrounding link of a clicked element.
+    $(event.target).closest("a,area").each(function() {
+
+      var ga = Drupal.settings.googleanalytics;
+      // Expression to check for special links like gotwo.module /go/* links.
+      var isInternalSpecial = new RegExp("(\/go\/.*)$", "i");
+      // Expression to check for download links.
+      var isDownload = new RegExp("\\.(" + ga.trackDownloadExtensions + ")$", "i");
+
+      // Is the clicked URL internal?
+      if (isInternal.test(this.href)) {
+        // Skip 'click' tracking, if custom tracking events are bound.
+        if ($(this).is('.colorbox')) {
+          // Do nothing here. The custom event will handle all tracking.
+        }
+        // Is download tracking activated and the file extension configured for download tracking?
+        else if (ga.trackDownload && isDownload.test(this.href)) {
+          // Download link clicked.
+          var extension = isDownload.exec(this.href);
+          _gaq.push(["_trackEvent", "Downloads", extension[1].toUpperCase(), this.href.replace(isInternal, '')]);
+        }
+        else if (isInternalSpecial.test(this.href)) {
+          // Keep the internal URL for Google Analytics website overlay intact.
+          _gaq.push(["_trackPageview", this.href.replace(isInternal, '')]);
+        }
+      }
+      else {
+        if (ga.trackMailto && $(this).is("a[href^='mailto:'],area[href^='mailto:']")) {
+          // Mailto link clicked.
+          _gaq.push(["_trackEvent", "Mails", "Click", this.href.substring(7)]);
+        }
+        else if (ga.trackOutbound && this.href.match(/^\w+:\/\//i)) {
+          if (ga.trackDomainMode == 2 && isCrossDomain($(this).attr('hostname'), ga.trackCrossDomains)) {
+            // Top-level cross domain clicked. document.location is handled by _link internally.
+            event.preventDefault();
+            _gaq.push(["_link", this.href]);
+          }
+          else {
+            // External link clicked.
+            _gaq.push(["_trackEvent", "Outbound links", "Click", this.href]);
+          }
+        }
+      }
+    });
+  });
+
+  // Colorbox: This event triggers when the transition has completed and the
+  // newly loaded content has been revealed.
+  $(document).bind("cbox_complete", function() {
+    var href = $.colorbox.element().attr("href");
+    if (href) {
+      _gaq.push(["_trackPageview", href.replace(isInternal, '')]);
+    }
+  });
+
+});
+
+/**
+ * Check whether the hostname is part of the cross domains or not.
+ *
+ * @param string hostname
+ *   The hostname of the clicked URL.
+ * @param array crossDomains
+ *   All cross domain hostnames as JS array.
+ *
+ * @return boolean
+ */
+function isCrossDomain(hostname, crossDomains) {
+  /**
+   * jQuery < 1.6.3 bug: $.inArray crushes IE6 and Chrome if second argument is
+   * `null` or `undefined`, http://bugs.jquery.com/ticket/10076,
+   * https://github.com/jquery/jquery/commit/a839af034db2bd934e4d4fa6758a3fed8de74174
+   *
+   * @todo: Remove/Refactor in D8
+   */
+  if (!crossDomains) {
+    return false;
+  }
+  else {
+    return $.inArray(hostname, crossDomains) > -1 ? true : false;
+  }
+}
+
+})(jQuery);
+;
+(function ($) {
+
+Drupal.toolbar = Drupal.toolbar || {};
+
+/**
+ * Attach toggling behavior and notify the overlay of the toolbar.
+ */
+Drupal.behaviors.toolbar = {
+  attach: function(context) {
+
+    // Set the initial state of the toolbar.
+    $('#toolbar', context).once('toolbar', Drupal.toolbar.init);
+
+    // Toggling toolbar drawer.
+    $('#toolbar a.toggle', context).once('toolbar-toggle').click(function(e) {
+      Drupal.toolbar.toggle();
+      // Allow resize event handlers to recalculate sizes/positions.
+      $(window).triggerHandler('resize');
+      return false;
+    });
+  }
+};
+
+/**
+ * Retrieve last saved cookie settings and set up the initial toolbar state.
+ */
+Drupal.toolbar.init = function() {
+  // Retrieve the collapsed status from a stored cookie.
+  var collapsed = $.cookie('Drupal.toolbar.collapsed');
+
+  // Expand or collapse the toolbar based on the cookie value.
+  if (collapsed == 1) {
+    Drupal.toolbar.collapse();
+  }
+  else {
+    Drupal.toolbar.expand();
+  }
+};
+
+/**
+ * Collapse the toolbar.
+ */
+Drupal.toolbar.collapse = function() {
+  var toggle_text = Drupal.t('Show shortcuts');
+  $('#toolbar div.toolbar-drawer').addClass('collapsed');
+  $('#toolbar a.toggle')
+    .removeClass('toggle-active')
+    .attr('title',  toggle_text)
+    .html(toggle_text);
+  $('body').removeClass('toolbar-drawer').css('paddingTop', Drupal.toolbar.height());
+  $.cookie(
+    'Drupal.toolbar.collapsed',
+    1,
+    {
+      path: Drupal.settings.basePath,
+      // The cookie should "never" expire.
+      expires: 36500
+    }
+  );
+};
+
+/**
+ * Expand the toolbar.
+ */
+Drupal.toolbar.expand = function() {
+  var toggle_text = Drupal.t('Hide shortcuts');
+  $('#toolbar div.toolbar-drawer').removeClass('collapsed');
+  $('#toolbar a.toggle')
+    .addClass('toggle-active')
+    .attr('title',  toggle_text)
+    .html(toggle_text);
+  $('body').addClass('toolbar-drawer').css('paddingTop', Drupal.toolbar.height());
+  $.cookie(
+    'Drupal.toolbar.collapsed',
+    0,
+    {
+      path: Drupal.settings.basePath,
+      // The cookie should "never" expire.
+      expires: 36500
+    }
+  );
+};
+
+/**
+ * Toggle the toolbar.
+ */
+Drupal.toolbar.toggle = function() {
+  if ($('#toolbar div.toolbar-drawer').hasClass('collapsed')) {
+    Drupal.toolbar.expand();
+  }
+  else {
+    Drupal.toolbar.collapse();
+  }
+};
+
+Drupal.toolbar.height = function() {
+  var $toolbar = $('#toolbar');
+  var height = $toolbar.outerHeight();
+  // In modern browsers (including IE9), when box-shadow is defined, use the
+  // normal height.
+  var cssBoxShadowValue = $toolbar.css('box-shadow');
+  var boxShadow = (typeof cssBoxShadowValue !== 'undefined' && cssBoxShadowValue !== 'none');
+  // In IE8 and below, we use the shadow filter to apply box-shadow styles to
+  // the toolbar. It adds some extra height that we need to remove.
+  if (!boxShadow && /DXImageTransform\.Microsoft\.Shadow/.test($toolbar.css('filter'))) {
+    height -= $toolbar[0].filters.item("DXImageTransform.Microsoft.Shadow").strength;
+  }
+  return height;
+};
+
+})(jQuery);
+;
---
+++ sites/default/files/js/wysiwyg/wysiwyg_ckeditor_nX1x3YESbYsyiga1AkLy3TYC68qVbtadDs839TAB-j8.js
@@ -0,0 +1,1 @@
+window.CKEDITOR_BASEPATH = '/pentafoods/public/sites/all/libraries/ckeditor/';
