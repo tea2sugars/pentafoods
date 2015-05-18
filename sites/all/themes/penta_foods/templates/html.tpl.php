12bd6bca5fdc230c29769a63c115d94c5ce3c33a
Rik Barwick <rik@creativitea.co.uk>
2015-05-18 11:12:54 +0100

    sync with live site

---
+++ sites/all/themes/penta_foods/templates/html.tpl.php
@@ -0,0 +1,26 @@
+<?php print $doctype; ?>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
+<head<?php print $rdf->profile; ?>>
+	<script type="text/javascript" src="http://mavic852.com/js/23035.js" ></script>
+	<noscript><img src="http://mavic852.com/images/track/23035.png?trk_user=23035&trk_tit=jsdisabled&trk_ref=jsdisabled&trk_loc=jsdisabled" height="0px" width="0px" style="display:none;" /></noscript>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>  
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <!--[if lt IE 7]>
+<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE7.js"></script>
+<![endif]-->
+  <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body<?php print $attributes;?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+  <script type="text/javascript" src="https://insight.mediaheads.co.uk/vi.js"></script> 
+  <script type="text/javascript">vi_track_pageview('pt6f-drb9');</script>
+</body>
+
+</html>
